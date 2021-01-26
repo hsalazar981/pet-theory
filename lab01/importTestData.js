bd5bf49c306567021d9d57427195d22ec62c7283
hsalazar981 <78034313+hsalazar981@users.noreply.github.com>
2021-01-26 12:17:25 -0300

    Update importTestData.js

--- lab01/importTestData.js
+++ lab01/importTestData.js
@@ -1,12 +1,28 @@
 const {promisify} = require('util');
 const parse       = promisify(require('csv-parse'));
 const {readFile}  = require('fs').promises;
-
+const {Firestore} = require('@google-cloud/firestore');
 if (process.argv.length < 3) {
   console.error('Please include a path to a csv file');
   process.exit(1);
 }
+const db = new Firestore();
 
+function writeToFirestore(records) {
+  const batchCommits = [];
+  let batch = db.batch();
+  records.forEach((record, i) => {
+    var docRef = db.collection('customers').doc(record.email);
+    batch.set(docRef, record);
+    if ((i + 1) % 500 === 0) {
+      console.log(`Writing record ${i + 1}`);
+      batchCommits.push(batch.commit());
+      batch = db.batch();
+    }
+  });
+  batchCommits.push(batch.commit());
+  return Promise.all(batchCommits);
+}
 function writeToDatabase(records) {
   records.forEach((record, i) => {
     console.log(`ID: ${record.id} Email: ${record.email} Name: ${record.name} Phone: ${record.phone}`);
